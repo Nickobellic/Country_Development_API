ba3ba63382d921e174ac772a5c7403becf12d188
NickoBellic <avrahulkanna17@gmail.com>
2022-10-29 13:25:03 +0530

    Initial Build

---
+++ main.py
@@ -0,0 +1,17 @@
+# THIS API RETURNS COUNTRY'S ECONOMIC, SOCIAL CONDITION DATA
+
+from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
